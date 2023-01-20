57af2e0321b4e07258156049dab613928ca976fe
Lucas Serral <lucasser21@gmail.com>
2023-01-19 21:47:23 -0300

    [ADD] interactive menu

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const { mostarMenu, pause } = require("./helpers/mensajes");
+import "colors";
+import { pause, inquirerMenu } from "./helpers/mensajes.js";
 
-require("colors");
 console.clear();
+
+const tareas = [];
 
 const main = async () => {
   console.log("hola mundo.");
 
+  let option;
   do {
-    let option = await mostarMenu();
-    if (option != 0) pause();
+    option = await inquirerMenu();
+    console.log(option);
+    if (option != 0) await pause();
   } while (option != 0);
 };
 
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,56 +1,23 @@
-const { resolve } = require("path");
+import inquirer from "inquirer";
+import "colors";
+import questions from "../lists/questions";
 
-require("colors");
-
-const mostarMenu = () => {
-  return new Promise((resolve, reject) => {
-    console.clear();
-    console.log("============".green);
-    console.log("elija opcion".green);
-    console.log("============\n".green);
-
-    console.log(`${"1.".green} Crear tarea`);
-    console.log(`${"2.".green} Listar tareas`);
-    console.log(`${"3.".green} Listar tareas completadas`);
-    console.log(`${"4.".green} Listar tareas pendientes`);
-    console.log(`${"5.".green} Completar tarea(s)`);
-    console.log(`${"6.".green} Borrar tarea(s)`);
-    console.log(`${"0.".green} Salir \n`);
-
-    const readLine = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-
-    let option;
-
-    readLine.question("Seleccione una opcion ", (opt) => {
-      console.log({ opt });
-      readLine.close();
-      resolve(opt);
-    });
-
-    return option;
-  });
+const pause = async () => {
+  await inquirer.prompt([
+    {
+      name: "pause",
+      type: "input",
+      message: "Presione 'ENTER' para continuar",
+    },
+  ]);
+  return;
 };
 
-const pause = () => {
-  return new Promise((resolve, reject) => {
-    const readLine = require("readline").createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+const inquirerMenu = async () => {
+  console.clear();
 
-    readLine.question(
-      `Presione ${"ENTER".green} para continuar...\n`,
-      (evt) => {
-        readLine.close();
-        evt == "" ? resolve("") : pause();
-      }
-    );
-  });
+  const { opt } = await inquirer.prompt(questions);
+  return opt;
 };
-module.exports = {
-  mostarMenu,
-  pause,
-};
+
+export { pause, inquirerMenu };
